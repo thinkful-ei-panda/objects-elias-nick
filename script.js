0077f077018ff7f883bfa1965cd6e93e061ae8e5
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-01 16:26:24 -0500

    LOTR

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 'use strict';
 
-let loaf = {
-  flour: 300,
-  water: 210,
-  hydration() { //if this was an arrow function 'this' would not work correctly
-    return this.water / this.flour * 100;
-  }
-};
+function createCharacter(name, nickname, race, origin, attack, defense) {
+  return {
+    name,
+    nickname,
+    race,
+    origin,
+    attack,
+    defense,
+    describe() {
+      return `${this.name} is a ${this.race} from ${this.origin}`;
+    },
+    calulateDamage(attack, defense) {
+      return defense > attack ? 0 : attack - defense;
+    },
+    evaluateFight(character) {
+      return `${character.nickname} takes ${this.calulateDamage(this.attack, character.defense)} and you receive ${this.calulateDamage(character.attack, this.defense)}`;
+    }
+  };
+}
 
-console.log(loaf.hydration());
+let gandalf = new createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
+let bilbo = new createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
+
+console.log(gandalf.evaluateFight(bilbo));
