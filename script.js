12c99ca680df548833b4e437cb313954dd4023ac
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-01 14:32:44 -0500

    Properties that aren't there

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 'use strict';
 
 let loaf = {
-  flour: 300,
-  water: 210,
-  hydration() { //if this was an arrow function 'this' would not work correctly
-    return this.water / this.flour * 100;
-  }
+    flour: 300,
+    water: 210,
+    hydration() { //if this was an arrow function 'this' would not work correctly
+        return this.water / this.flour * 100;
+    }
 };
 
-function People(name,jobTitle) {
-  this.name = name;
-  this.jobTitle = jobTitle;
+function People(name, jobTitle, boss) {
+    this.name = name;
+    this.jobTitle = jobTitle;
+    this.boss = boss;
 }
 
 let people = [
-  new People('martin','First-Man'),
-  new People('marty','CEO'),
-  new People('jerry','Accountant')
+    new People('martin', 'First-Man', 'marty'),
+    new People('marty', 'CEO'),
+    new People('jerry', 'Accountant', 'marty')
 ];
 
-for (let i=0; i<3;i++){
-  console.log(people[i].name +' '+ people[i].jobTitle);
+for (let i = 0; i < 3; i++) {
+    if (typeof people[i].boss === 'string')
+        console.log(`${people[i].name} ${people[i].jobTitle} reports to ${people[i].boss}`);
+    else
+        console.log(`${people[i].name} ${people[i].jobTitle} doesn't report to anybody.`);
 }
 
