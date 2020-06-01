ac7f3327f78b669fe867e0e12e02dd1a5735c3bb
Elias Martinez <jemart218@gmail.com>
2020-06-01 12:37:20 -0700

    properties not there

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 'use strict';
 
 let loaf = {
-    flour: 300,
-    water: 210,
-    hydration() { //if this was an arrow function 'this' would not work correctly
-        return this.water / this.flour * 100;
-    }
+  flour: 300,
+  water: 210,
+  hydration() { //if this was an arrow function 'this' would not work correctly
+    return this.water / this.flour * 100;
+}
 };
 
 function People(name, jobTitle, boss) {
-    this.name = name;
-    this.jobTitle = jobTitle;
-    this.boss = boss;
+  this.name = name;
+  this.jobTitle = jobTitle;
+  this.boss = boss;
 }
 
 let people = [
-    new People('martin', 'First-Man', 'marty'),
-    new People('marty', 'CEO'),
-    new People('jerry', 'Accountant', 'marty')
+  new People('martin', 'First-Man', 'marty'),
+  new People('marty', 'CEO'),
+  new People('jerry', 'Accountant', 'marty')
 ];
 
 for (let i = 0; i < 3; i++) {
-    if (typeof people[i].boss === 'string')
-        console.log(`${people[i].name} ${people[i].jobTitle} reports to ${people[i].boss}`);
-    else
-        console.log(`${people[i].name} ${people[i].jobTitle} doesn't report to anybody.`);
+  if (typeof people[i].boss === 'string')
+    console.log(`${people[i].name} ${people[i].jobTitle} reports to ${people[i].boss}`);
+  else
+    console.log(`${people[i].name} ${people[i].jobTitle} doesn't report to anybody.`);
 }
 
