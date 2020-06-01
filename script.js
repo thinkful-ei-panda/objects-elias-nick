37f0a736b3f6b1fd5bcab4c2276caa25aa1dfdee
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-01 11:31:25 -0500

    Object initializers and methods

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 let loaf = {
   flour: 300,
-  water: 210
+  water: 210,
+  hydration() { //if this was an arrow function 'this' would not work correctly
+    return this.water / this.flour * 100;
+  }
 };
 
-console.log(loaf.flour);
-console.log(loaf.water);
-
-
+console.log(loaf.hydration());
