086f48609c6470c2e703bfb8815e56da652a09e8
Elias Martinez <jemart218@gmail.com>
2020-06-01 13:56:24 -0700

    decoded message

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let loaf = {
   flour: 300,
   water: 210,
@@ -7,5 +7,24 @@ let loaf = {
     return this.water / this.flour * 100;
   }
 };
+*/
 
-console.log(loaf.hydration());
+function decode(encodedWord) {
+  const string = encodedWord[0];
+  const cipher = {a: 1, b: 2, c: 3, d:4};
+  if (string in cipher === false) {
+    return ' ';
+  }
+  return encodedWord[cipher[string]];
+}
+
+function decodeWords(message) {
+  let decoded = '';
+  const array = message.split(' ');
+  for (let i=0; i<array.length; i++) {
+    decoded += decode(array[i]);
+  }
+  return console.log(decoded);
+}
+
+decodeWords('craft block argon meter bells brown croon droop');
