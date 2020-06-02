886a45c686e40bc0635412e1fa1f2c4ccf374330
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-01 19:03:07 -0500

    BONUS II: A Database Method Initial

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 'use strict';
 
-let loaf = {
-  flour: 300,
-  water: 210,
-  hydration() { //if this was an arrow function 'this' would not work correctly
-    return this.water / this.flour * 100;
-  }
-};
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
 
-console.log(loaf.hydration());
+function findOne(arr, query) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let key in arr[i]) {
+      if (key in query) {
+        return arr[i].name;
+      }
+    }
+  }
+  return null;
+}
+
+console.log(findOne(HEROES, { id: 10 }));
